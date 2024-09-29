61c4a5c5efd7c9b50ccf58136f4becb2d769ad5a
tech nerd <yeabnoah5@gmail.com>
2024-09-29 06:52:58 +0300

    fixed the fav icon and also app description

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,4 +1,6 @@
 import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
   return (
@@ -6,10 +8,20 @@ export default function Header() {
       initial={{ opacity: 0, y: -50 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="w-full flex justify-between items-center mb-8 sm:mb-12"
+      className="w-full flex justify-between items-center mb-8 sm:mb-12 px-4 sm:px-6 py-4"
     >
-      <div className="text-xl sm:text-2xl font-bold text-white">Nerdspace</div>
-      <button className="bg-purple-600 px-4 py-3 rounded-full text-sm font-bold hover:bg-purple-700 transition-colors duration-300">
+      <Link href="/" className="flex items-center space-x-2 px-3 rounded">
+        <Image
+          className="rounded"
+          src="/nerdspace.png"
+          alt="Nerdspace-logo"
+          height={30}
+          width={30}
+        />
+        <span className="text-xl sm:text-2xl font-bold text-white">Nerdspace</span>
+      </Link>
+
+      <button className="bg-purple-600 px-4 py-2 rounded-full text-sm font-bold hover:bg-purple-700 transition-colors duration-300">
         Sign In
       </button>
     </motion.header>
